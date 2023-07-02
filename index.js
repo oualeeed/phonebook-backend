3764b86774463491de6fca9b12e8f630e85cfa30
Oualid elf <oualid.elferaoui@gmail.com>
2023-07-02 12:52:45 +0100

    the phonebook project to push to github  (accomplished the step 9 in the course )

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons =  [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+ 
+
+app.get('/api/persons/' , (request , response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id' ,(request , response )=> {
+  const id = Number(request.params.id)
+  const note = persons.find( person => person.id === id )
+  if(note){
+    response.json(note)
+  }else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id' ,(request , response )=> {
+  const id = Number(request.params.id)
+  persons = persons.filter( person => person.id !== id )
+  response.status(204).end()
+})
+
+
+app.get('/info/' , (request, response ) => {
+  response.send(
+    "<p>phone book has info for 2 peopple</p><p id='a'></p> <script> document.getElementById('a').innerHTML = new Date()</script>"
+  )
+})
+
+app.post('/api/persons' , (request, response)=>{
+  const body = request.body 
+  // console.log(body)
+
+  if( !body.name ) {
+    return response.status(400).json({
+      error: "name not specified"
+    })
+  }
+
+  if( !body.number ) {
+    return response.status(400).json({
+      error: "number not specified"
+    })
+  }
+
+  const identicallPerson = persons.find(person => person.name === body.name)
+  if(identicallPerson){
+    return response.status(400).json({
+      error : "This name is already in the database."
+    })
+  }
+
+  const person = {
+    id : generateId(),
+    name : body.name, 
+    number : body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const generateId = ()=> Math.floor(Math.random() * 100000)
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
