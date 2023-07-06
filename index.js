84f143e795585a77ddc9a0799f4ef5ceed67348b
Oualid elf <oualid.elferaoui@gmail.com>
2023-07-06 20:10:01 +0100

    backend fetches data from MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const cors = require("cors")
 const express = require("express")
 const morgan = require("morgan")
-const cors = require("cors")
+const Person = require('./models/person.js')
 
 const app = express()
 
@@ -14,33 +15,38 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons =  [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons =  [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
  
 
 app.get('/api/persons/' , (request , response) => {
-  response.json(persons)
+  Person.find({}).then(
+    persons =>{ 
+      console.log(persons)
+      response.json(persons)
+    }
+  )
 })
 
 app.get('/api/persons/:id' ,(request , response )=> {
